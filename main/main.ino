13546e348b26d0a807ab0d6a9c6a10c79891c5bf
Zooker1234 <ari.khavulya@gmail.com>
2022-11-16 19:22:58 -0500

    made rgb code a class

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,16 @@
 #include "drivecode.h"
+#include "rgbsensorcode.h"
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+RGBSensor rgb = new RGBSensor();
 
 void setup() {
-  
-  }
+  Serial.begin(9600);
+  rgb.isConnected();
+}
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  rgb.printResults();
 }
--- main/rgbsensorcode.h
+++ main/rgbsensorcode.h
@@ -1,48 +1,52 @@
-#include <Wire.h>
-#include "Adafruit_TCS34725.h"
+public class RGBSensor {
+  #include <Wire.h>
+  #include "Adafruit_TCS34725.h"
+  
+  private red;
+  private green;
+  private blue;
+  private co; //c
+  private l; //lux
+  private cT; //colortemp
+  private Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+  
+  
+  /* Example code for the Adafruit TCS34725 breakout library */
+  
+  /* Connect SCL    to SCL (pin 21)
+     Connect SDA    to SDA (pin 20)
+     Connect VDD    to 3.3V DC
+     Connect GROUND to common ground */
+  
+  /* Initialise with default values (int time = 2.4ms, gain = 1x) */
+  // Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+  
+  /* Initialise with specific int time and gain values */
 
-private 
-
-//
-/* Example code for the Adafruit TCS34725 breakout library */
-
-/* Connect SCL    to SCL (pin 21)
-   Connect SDA    to SDA (pin 20)
-   Connect VDD    to 3.3V DC
-   Connect GROUND to common ground */
-
-/* Initialise with default values (int time = 2.4ms, gain = 1x) */
-// Adafruit_TCS34725 tcs = Adafruit_TCS34725();
-
-/* Initialise with specific int time and gain values */
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
-//
-void setup(void) {
-  Serial.begin(9600);
-
-  if (tcs.begin()) {
-    Serial.println("Found sensor");
-  } else {
-    Serial.println("No TCS34725 found ... check your connections");
-    while (1);
+  public RGBSensor(){}
+  
+  public void isConnected(){
+    if (tcs.begin()) {
+      Serial.println("Found sensor");
+    } 
+    else {
+      Serial.println("No TCS34725 found ... check your connections");
+    }
   }
-
-  // Now we're ready to get readings!
-}
-
-void loop(void) {
-  uint16_t r, g, b, c, colorTemp, lux;
-
-  tcs.getRawData(&r, &g, &b, &c);
-  // colorTemp = tcs.calculateColorTemperature(r, g, b);
-  colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
-  lux = tcs.calculateLux(r, g, b);
-
-  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-  Serial.println(" ");
+  void printResults() {
+      
+    uint16_t r, g, b, c, colorTemp, lux;
+    tcs.getRawData(&r, &g, &b, &c);
+    // colorTemp = tcs.calculateColorTemperature(r, g, b);
+    colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+    lux = tcs.calculateLux(r, g, b);
+  
+    Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+    Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+    Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+    Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+    Serial.println(" ");
+   }
 }
