baa40f383de5e79812200c3e847ec4e87270e038
JackTheFine <112029550+JackTheFine@users.noreply.github.com>
2022-11-17 08:01:08 -0500

    added fake driving code and pin settings cuz it works

--- main/drivecode.h
+++ main/drivecode.h
@@ -1,4 +1,20 @@
 
 void drive() {
   
+  //digitalWrite(motor1pin1, HIGH);
+  //digitalWrite(motor1pin2, LOW);
+  //digitalWrite(motor2pin1, HIGH);
+  //digitalWrite(motor2pin2, LOW);
+  //digitalWrite(motor1pin1, LOW);
+  //digitalWrite(motor1pin2, HIGH);
+  //digitalWrite(motor2pin1, LOW);
+  //digitalWrite(motor2pin2, HIGH);
+  //digitalWrite(motor3pin1, HIGH);
+  //digitalWrite(motor3pin2, LOW);
+  //digitalWrite(motor4pin1, HIGH);
+  //digitalWrite(motor4pin2, LOW);
+  //digitalWrite(motor3pin1, LOW);
+  //digitalWrite(motor3pin2, HIGH);
+  //digitalWrite(motor4pin1, LOW);
+  //digitalWrite(motor4pin2, HIGH);
 }
--- main/main.ino
+++ main/main.ino
@@ -4,50 +4,35 @@
 #include "Adafruit_TCS34725.h"
 RGBSensor rgb = new RGBSensor();
 
-int m1p1 = 1;
-int m1p2 = 2;
-int enA = 9;
-//motor 2 - right on L298N - front right motor
-int m2p1 = 3;
-int m2p2 = 4;
-int enB = 10;
-//motor 3 - left on L298N - back right motor
-int m3p1 = 5;
-int m3p2 = 6;
-int enC = 11;
-//motor 4 - right on L298N - back left motor
-int m4p1 = 7;
-int m4p2 = 8;
-int enD = 12;
-//speed, angle, and rotation to be varied
-double gSpeed;
-double gAngle;
-double gRotation;
+  int motor1pin1 = 1;
+int motor1pin2 = 2;
 
-double drivex(double speed, double angle, double rotation) {
-  angle = radians(angle);
-  return (speed * sin(angle - 45) + rotation );
-}
+int motor2pin1 = 3;
+int motor2pin2 = 4;
 
-double drivey(double speed, double angle, double rotation) {
-  angle = radians(angle);
-  return (speed * sin(angle + 45) + rotation );
-}
+int motor3pin1 = 5;
+int motor3pin2 = 6;
 
+int motor4pin1 = 7;
+int motor4pin2 = 8;
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor2pin1, OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
+  pinMode(motor3pin1, OUTPUT);
+  pinMode(motor3pin2, OUTPUT);
+  pinMode(motor4pin1, OUTPUT);
+  pinMode(motor4pin2, OUTPUT);
 
 void setup() {
-  pinMode(m1p1, OUTPUT);
-  pinMode(m1p2, OUTPUT);
-  pinMode(m2p1, OUTPUT);
-  pinMode(m2p2, OUTPUT);
-  pinMode(m3p1, OUTPUT);
-  pinMode(m3p2, OUTPUT);
-  pinMode(m4p1, OUTPUT);
-  pinMode(m4p2, OUTPUT);
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(enC, OUTPUT);
-  pinMode(enD, OUTPUT);
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor2pin1, OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
+  pinMode(motor3pin1, OUTPUT);
+  pinMode(motor3pin2, OUTPUT);
+  pinMode(motor4pin1, OUTPUT);
+  pinMode(motor4pin2, OUTPUT);
   Serial.begin(9600);
   rgb.isConnected();
 }
