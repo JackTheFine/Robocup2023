9bde9eaadffccbb8e053f001734337e1001948b7
JackTheFine <112029550+JackTheFine@users.noreply.github.com>
2022-11-22 09:07:45 -0500

    Update main.ino
    Changed all pins one higher b/c pin 1 is always powered for some reason also added test mode and configured the motor movements with functions

--- main/main.ino
+++ main/main.ino
@@ -45,18 +45,18 @@ class RGBSensor {
 
 //RGBSensor rgb = new RGBSensor();
 
-int motor1pin1 = 1;
-int motor1pin2 = 2;
-
-int motor2pin1 = 3;
-int motor2pin2 = 4;
-
-int motor3pin1 = 5;
-int motor3pin2 = 6;
-
-int motor4pin1 = 7;
-int motor4pin2 = 8;
-
+int motor1pin1 = 2;
+int motor1pin2 = 3;
+int spa = 10;
+int motor2pin1 = 4;
+int motor2pin2 = 5;
+int spb = 11;
+int motor3pin1 = 6;
+int motor3pin2 = 7;
+int spc = 12;
+int motor4pin1 = 8;
+int motor4pin2 = 9;
+int spd = 13;
 void setup() {
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
@@ -90,32 +90,53 @@ void backward() {
 };
 
 void left() {
-  
-  //digitalWrite(motor1pin1, HIGH);
-  //digitalWrite(motor1pin2, LOW);
-  //digitalWrite(motor2pin1, HIGH);
-  //digitalWrite(motor2pin2, LOW);
-  //digitalWrite(motor3pin1, HIGH);
-  //digitalWrite(motor3pin2, LOW);
-  //digitalWrite(motor4pin1, HIGH);
-  //digitalWrite(motor4pin2, LOW);
+
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+  digitalWrite(motor4pin1, HIGH);
+  digitalWrite(motor4pin2, LOW);
   
 }
 
 void right() {
   
-  //digitalWrite(motor1pin1, HIGH);
-  //digitalWrite(motor1pin2, LOW);
-  //digitalWrite(motor2pin1, HIGH);
-  //digitalWrite(motor2pin2, LOW);
-  //digitalWrite(motor3pin1, HIGH);
-  //digitalWrite(motor3pin2, LOW);
-  //digitalWrite(motor4pin1, HIGH);
-  //digitalWrite(motor4pin2, LOW);
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
+  digitalWrite(motor4pin1, LOW);
+  digitalWrite(motor4pin2, HIGH);
+  
+}
+
+void motortest() {
+
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  Serial.println("Motor two shoukld be moving");
+  delay(1000);
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+  Serial.println("Motor two shoukld be moving");
+  delay(1000);
+  digitalWrite(motor3pin1, HIGH);
+  digitalWrite(motor3pin2, LOW);
+  Serial.println("Motor two shoukld be moving");
+  delay(1000);
+  digitalWrite(motor4pin1, HIGH);
+  digitalWrite(motor4pin2, LOW);
+  Serial.println("Motor two shoukld be moving");
+  delay(1000);
+  
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
  foward();
+ delay(10000);
+ backward();
+ delay(10000);
+ left();
+ delay(10000);
+ right();
+ delay(10000);
 }
