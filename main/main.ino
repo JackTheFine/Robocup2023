336d6106b545c353975e62ceee35f3d3b4ff0455
Zooker1234 <mysterymann4169@gmail.com>
2022-11-30 18:59:54 -0500

    added RBG sensor function

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,23 @@
-//returns true if robot can pass, false if can't
-bool rgbColor(sensorNumber){
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+//returns true if all sensors see green, false if a sensor sees white
+bool rgbColor(){
+  const int threshold = 50;
+  uint16_t r, g, b, c, colorTemp, lux;
 
+  tcs.getRawData(&r, &g, &b, &c);
+  // colorTemp = tcs.calculateColorTemperature(r, g, b);
+  colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+  lux = tcs.calculateLux(r, g, b);
+  lux = lux/256;
+  if (lux > threshold) {
+    return false;
+  }
+  return true;
 }
+
+
 
 void setup() {
   // put your setup code here, to run once:
